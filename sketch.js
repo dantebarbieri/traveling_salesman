56652217415b4f2b8d4d8d46aa49677cb467270d
Dante Barbieri <dantevbarbieri@gmail.com>
2018-05-02 12:20:38 -0500

    Add points

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
+let points = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 10; i++) {
+    points.push(createVector(random(width / 10, 9 * width / 10), random(height / 10, 9 * height / 10)));
+  }
 }
 
 function draw() {
   background(0);
-  rect(mouseX, mouseY, mouseY, mouseX);
+  for (let point of points) {
+    ellipse(point.x, point.y, 25);
+  }
 }
